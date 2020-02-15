82b1db539fc14a53c857c8c2ae4e3b1459ff1a3f
Felipe Castilhos <felipecastilhos@gmail.com>
2020-02-14 23:11:29 -0300

    add brave browser setup script

---
+++ setup-brave-browser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "###Starting to download Browser: Brave"
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -;
+
+sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com `lsb_release -sc` main" >> /etc/apt/sources.list.d/brave.list';
+
+apt update;
+
+sudo apt install brave-browser brave-keyring;
