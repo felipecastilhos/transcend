ed9f9f9ee79a6078a7989aa88579b2f39a23228f
Felipe Castilhos <felipecastilhos@gmail.com>
2020-02-15 00:34:14 -0300

    handle kvm support while instaling android studio

--- setup-android-studio.sh
+++ setup-android-studio.sh
@@ -2,6 +2,15 @@
 
 echo "###Starting to install Android Studio"
 
-sudo apt-add-repository ppa:paolorotolo/android-studio;
-sudo apt-get update;
-sudo apt-get install -y android-studio;
+apt-add-repository ppa:paolorotolo/android-studio;
+apt-get update;
+apt-get install -y android-studio;
+
+echo "###[Android Studio Instalation] Checking for KVM Support"
+apt-get install cpu-checker
+kvm-ok
+
+if [ $? -eq 0 ]
+then
+    apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils ia32-libs-multiarch
+fi
