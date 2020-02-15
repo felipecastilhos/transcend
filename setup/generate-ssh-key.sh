12008dc96ff7b58beedcf003792fbb52535adf6c
Felipe Castilhos <felipecastilhos@gmail.com>
2020-02-15 01:04:15 -0300

    add argument validation to generate-ssh-key

--- setup/generate-ssh-key.sh
+++ setup/generate-ssh-key.sh
@@ -1,8 +1,9 @@
 if [ $1 -eq 0]
 then
     echo "Digite seu email, para configurar a chave do SSH"
+    exit 1
 fi
 
-ssh-keygen -t rsa -b 4096 -C "felipecastilhos@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "$1"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
