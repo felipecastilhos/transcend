2a9d8e3630f2462363213ac59535fcfba4c90206
Felipe Castilhos <felipecastilhos@gmail.com>
2020-02-15 00:37:07 -0300

    moving setup files to setup folder

--- pimp-my-ubuntu.sh
+++ pimp-my-ubuntu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SETUP DEV TOOLS
-./setup-base-dev-tools rxvt-unicode-256color 
-./setup-android-dev-tools.sh
+setup/setup-base-dev-tools.sh rxvt-unicode-256color 
+setup/setup-android-dev-tools.sh
 
 #SETUP DEV PAYED TOOLS
-./setup-git-kraken.sh
-./setup-charles-proxy.sh
+setup/setup-git-kraken.sh
+setup/setup-charles-proxy.sh
 
 #SYSTEM BASIC SETUP
-./setup-browser.sh
-./setup-vscode.sh
+setup/setup-browser.sh
+setup/setup-vscode.sh
