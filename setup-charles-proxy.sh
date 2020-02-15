f8373e12c7a72fa20eb5813561ef150a3dbd07ce
Felipe Castilhos <felipecastilhos@gmail.com>
2020-02-14 23:31:53 -0300

    update charles script

--- setup-charles-proxy.sh
+++ setup-charles-proxy.sh
@@ -2,6 +2,8 @@
 
 echo "###Starting to install Charles Proxy"
 
+sudo apt-get install gconf2 gconf2-service python;
+
 wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -;
 sudo sh -c 'echo deb https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list';
 sudo apt-get update;
